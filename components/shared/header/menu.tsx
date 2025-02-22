d11db03ee781dad076d00d8dec5f4a15a424cd48
Rakib Roni <rakibronicse@gmail.com>
2025-02-22 22:11:24 +0100

    add header component

--- components/shared/header/menu.tsx
+++ components/shared/header/menu.tsx
@@ -1,21 +1,20 @@
 import { ShoppingCartIcon, UserIcon } from 'lucide-react'
 import Link from 'next/link'
 
-const Menu = () => {
+export default function Menu() {
   return (
     <div className="flex justify-end">
       <nav className="flex gap-3 w-full">
         <Link href="/cart" className="header-button">
-          <UserIcon className="w-8 h-8" />
+          <UserIcon className="h-8 w-8" />
           <span className="font-bold">Sign in</span>
         </Link>
-        <link href="/cart" className="header-button">
-          <ShoppingCartIcon className="w-8 h-8" />
+
+        <Link href="/cart" className="header-button">
+          <ShoppingCartIcon className="h-8 w-8" />
           <span className="font-bold">Cart</span>
-        </link>
+        </Link>
       </nav>
     </div>
   )
 }
-
-export default Menu
