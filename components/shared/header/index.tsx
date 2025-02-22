c76aa89004f543e08b457e8141bcad5ae120c673
Rakib Roni <rakibronicse@gmail.com>
2025-02-22 23:36:16 +0100

    cart icon redesign

--- components/shared/header/index.tsx
+++ components/shared/header/index.tsx
@@ -30,7 +30,7 @@ export default function Header() {
       <div className="flex items-center px-3 mb-[1px]  bg-gray-800">
         <Button
           variant="ghost"
-          className="header-button flex items-center gap-1 text-base [&_svg]:size-6"
+          className="dark header-button flex items-center gap-1 text-base [&_svg]:size-6"
         >
           <MenuIcon />
           All
